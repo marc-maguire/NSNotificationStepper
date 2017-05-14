6a2fc959114ef00054caf3733f8b815495c4ee51
Marc Maguire <maguire.marc@gmail.com>
2017-05-14 09:56:28 -0400

    initial commit

---
+++ NSNotificationActivity/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NSNotificationActivity
+//
+//  Created by Marc Maguire on 2017-05-14.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
